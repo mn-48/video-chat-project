03ae134942930c6c03036e1ab3f99b68ec56776e
mn-48 <nazmul.cse48@gmail.com>
2024-06-04 17:03:41 +0600

    app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("It works!!");
+import express from "express";
+import http from "http";
+
+const port = 8000;
+
+const app = express;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Listening to the server | ${port}`);
+});
